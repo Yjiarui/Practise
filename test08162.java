cf8e0a4325cbb990b8061f4802d323eece7e63da
Yjiarui <523683998@qq.com>
2019-08-17 09:16:54 +0800

    输出顺序

---
+++ test08162.java
@@ -0,0 +1,42 @@
+class Parent{
+	{
+		System.out.println("A");
+	}
+	
+	Parent(){
+		System.out.println("B");
+	}
+	{
+		System.out.println("C");
+	}
+}
+public class InstanceOrder extends Parent{
+	InstanceOrder(){
+		super();
+		System.out.println(1);
+	}
+	InstanceOrder(int v){
+		this();
+		System.out.println(2);
+	}
+	private int initA(){
+		System.out.println(3);
+		return 0;
+	}
+	private int initB(){
+		System.out.println(4);
+		return 0;
+	}
+	int a = initA();
+	{
+	    System.out.println(5);	
+	}
+	int b = initB();
+	{
+		System.out.println(6);
+	}
+	public static void main(String[] args){
+		new InstanceOrder(1000);
+	}
+}
+//A,C,B,3,5,4,6,1,2
