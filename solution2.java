afb16f9e5752a3ac9b15fb30d6f8821280e54275
Yjiarui <523683998@qq.com>
2019-08-19 15:59:35 +0800

    杨辉三角

---
+++ solution2.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution2 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>(numRows);
+        //list是一种引用，List类型的接口引用（java语法）
+        //list逻辑上是一种线性表
+        //线性表的元素类型是List<Integer>
+        //   (java 语法) List 类型的接口引用
+        //               元素类型是 Integer 类类型的引用
+        //               Integer 是 int 的包装类
+        for(int i = 0;i < numRows;i++){
+            list.add(new ArrayList<>(i+1));
+        }
+        list.get(0).add(1);
+        list.get(1).add(1);
+        list.get(1).add(1);
+        for(int i = 2;i < numRows;i++){
+            List<Integer> nums = list.get(i);
+            nums.add(1);
+            for(int j = 1;j < i;j++){
+                int num = list.get(i-1).get(j-1)+list.get(i-1).get(j);
+                nums.add(num);
+            }
+            nums.add(1);
+        }
+        return  list;
+    }
+     private  static  void printPasscalsTriangle(Solution2 s,int n){
+         System.out.println(s.generate(n));
+     }
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        printPasscalsTriangle(solution,10);
+    }
+
+}
