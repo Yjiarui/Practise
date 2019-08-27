51971b12f5fd6a277faa30f26924504b6b97e404
Yjiarui <523683998@qq.com>
2019-08-27 14:56:38 +0800

    SDemo

---
+++ SDemo.java
@@ -0,0 +1,12 @@
+public class SDemo {
+    public static void main(String[] args) {
+        StringBuilder sb =  new StringBuilder();
+
+        sb.append("I");
+        sb.append("Love");
+        sb.append("Java");
+
+        String s = "I"+"Love"+"Java";
+        System.out.println(sb.toString());
+    }
+}
