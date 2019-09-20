3d34e7e1f1d072a3a79951d50e6716a47a44a266
Yjiarui <523683998@qq.com>
2019-09-20 12:25:16 +0800

    三个树之和

---
+++ ThreeSum.java
@@ -0,0 +1,124 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums){
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int i = 0;
+        while (i < nums.length - 2) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                while (j < k && nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                }
+                if (j >= k) {
+                    break;
+                }
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    List<Integer> e = new ArrayList<>();
+                    e.add(nums[i]);
+                    e.add(nums[j]);
+                    e.add(nums[k]);
+                    result.add(e);
+
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    j++;
+                }
+
+                while (j < k && nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                }
+                if (j >= k) {
+                    break;
+                }
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    List<Integer> e = new ArrayList<>();
+                    e.add(nums[i]);
+                    e.add(nums[j]);
+                    e.add(nums[k]);
+                    result.add(e);
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    k--;
+                }
+            }
+            while (i < nums.length - 2 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+            i++;
+        }
+
+        return result;
+    }
+    public static List<List<Integer>> threeSum2(int[] nums){
+        HashSet<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 2; i++){
+              int j = i + 1;
+              int k = nums.length - 1;
+              while (j < k){
+                  int n = nums[i] + nums[j] + nums[k];
+                  if(n < 0){
+                      j++;
+                  }else if(n > 0){
+                      k--;
+                  }else {
+                      List<Integer> e = new ArrayList<>();
+                      e.add(nums[i]);
+                      e.add(nums[j]);
+                      e.add(nums[k]);
+                      result.add(e);
+                  }
+              }
+        }
+        return new ArrayList<>(result);
+    }
+
+    public static List<List<Integer>> threeSum3(int[] nums){
+        Arrays.sort(nums);
+        List<List<Integer>> result =new ArrayList<>();
+        for (int i = 0 ; i < nums.length - 2;i++){
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k){
+                int s = nums[i] + nums[j] + nums[k];
+                if(s < 0){
+                    j++;
+                }else if(s > 0){
+                    k--;
+                }else {
+                    List<Integer> e = new ArrayList<>();
+                    e.add(nums[i]);
+                    e.add(nums[j]);
+                    e.add(nums[k]);
+                    result.add(e);
+                    while (j < k && nums[i]  == nums[j + 1]){
+                        j++;
+                    }
+                    j++;
+                    while (j < k && nums[k] == nums[k - 1]){
+                        k--;
+                    }
+                    k--;
+                }
+            }
+            while (i < nums.length - 2 && nums[i] == nums[i + 1]){
+                i++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> r = threeSum(nums);
+        System.out.println(r);
+    }
+}
