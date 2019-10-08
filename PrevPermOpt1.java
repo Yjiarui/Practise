fd9bcf208d428d92b34cd943e23898bef0126c52
Yjiarui <523683998@qq.com>
2019-10-08 21:31:23 +0800

    PrevPermOpt1

---
+++ PrevPermOpt1.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class PrevPermOpt1 {
+    public static int[] prevPermOpt1(int[] A){
+        for(int i = A.length - 2;i >= 0;i--){
+            if(A[i] > A[i + 1]){
+                int max = i + 2;
+                for (int j = i + 2;j < A.length;j++){
+                    if (A[j] < A[i] && A[j] > A[max]){
+                        max = j;
+                    }
+                }
+                int t = A[i];
+                A[i] = A[max];
+                A[max] = t;
+                return A;
+            }
+        }
+
+        return A;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {3,1,1,3};
+        System.out.println(Arrays.toString(prevPermOpt1(A)));
+    }
+}
